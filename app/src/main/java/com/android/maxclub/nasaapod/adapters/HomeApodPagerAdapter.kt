c015bcdb752f8d4a31d8f87aba55c333bf3c5bbe
MaxKliuba <klyubamaks@gmail.com>
2022-07-22 18:32:16 +0300

    upd PagerAdapter

--- app/src/main/java/com/android/maxclub/nasaapod/adapters/HomeApodPagerAdapter.kt
+++ app/src/main/java/com/android/maxclub/nasaapod/adapters/HomeApodPagerAdapter.kt
@@ -1,23 +1,45 @@
 package com.android.maxclub.nasaapod.adapters
 
-import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DiffUtil
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.android.maxclub.nasaapod.data.ApodDate
 import com.android.maxclub.nasaapod.fragments.ApodFragment
 
 class HomeApodPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
-    var currentList: List<ApodDate> = emptyList()
-        private set
+    private val currentList: MutableList<ApodDate> = mutableListOf()
 
     override fun getItemCount(): Int = currentList.size
 
     override fun createFragment(position: Int): Fragment =
         ApodFragment.newInstance(currentList[position])
 
-    @SuppressLint("NotifyDataSetChanged")
-    fun submitList(list: List<ApodDate>) {
-        currentList = list
-        notifyDataSetChanged()
+    override fun getItemId(position: Int): Long =
+        currentList[position].id.leastSignificantBits
+
+    override fun containsItem(itemId: Long): Boolean =
+        currentList.any { it.id.leastSignificantBits == itemId }
+
+    fun submitList(newList: List<ApodDate>) {
+        val callback = PagerDiffUtil(currentList, newList)
+        val diff = DiffUtil.calculateDiff(callback)
+        currentList.clear()
+        currentList.addAll(newList)
+        diff.dispatchUpdatesTo(this)
     }
+}
+
+class PagerDiffUtil(
+    private val oldList: List<ApodDate>,
+    private val newList: List<ApodDate>,
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldList[oldItemPosition].id == newList[newItemPosition].id
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldList[oldItemPosition].date == newList[newItemPosition].date
 }
